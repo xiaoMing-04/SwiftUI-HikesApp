474fb77f2a8927e20a5afca4df44e9de58fffc2e
Minh <minhnguyen47431@gmail.com>
2024-08-18 10:46:17 +0700

    Initial Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Nguyễn Minh on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
